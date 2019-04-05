1ff2da5310b9f1ce055f1eaab6917208d690a700
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2019-04-05 13:57:27 +0200

    use stencil components

--- src/app/canvas/containers/canvas/canvas.component.spec.ts
+++ src/app/canvas/containers/canvas/canvas.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 import { CanvasImageComponent, ColorPickerComponent } from '../../components';
+import { DrawingToolbarComponent } from '../../components/drawing-toolbar/drawing-toolbar.component';
 import { DrawDirective } from '../../directives/draw.directive';
 
 import { CanvasComponent } from './canvas.component';
@@ -16,6 +17,7 @@ describe('CanvasComponent', () => {
         CanvasComponent,
         ColorPickerComponent,
         CanvasImageComponent,
+        DrawingToolbarComponent,
         DrawDirective
       ],
       imports: [SharedModule]
