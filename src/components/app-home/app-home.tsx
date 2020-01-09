0ec8c85dac5930cbdd73fdb92a80f6aeadf42eee
Elger van Boxtel <elgervb@users.noreply.github.com>
2020-01-09 15:39:32 +0100

    show uploaded image on canvas (#14)
    * show uploaded image on canvas
    
    closes #10
    
    * remove console log statement
    
    #10

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -1,5 +1,7 @@
 import { Component, h } from '@stencil/core';
 import { PickedFile } from '@elgervb/stencil-components/dist/types/components/file-picker/pickedfile';
+import { createImage } from '../../global/utils/imgman_lib/create/image';
+import { clearCanvas } from '../../global/utils/imgman_lib/canvas/clear';
 
 @Component({
   tag: 'app-home',
@@ -8,28 +10,45 @@ import { PickedFile } from '@elgervb/stencil-components/dist/types/components/fi
 })
 export class AppHome {
 
+  private canvas: HTMLCanvasElement;
+
   private filePicked(upload: PickedFile) {
-    console.log('Uploaded', upload.file.name)
+
+    createImage(upload.dataUrl)
+      .onload = event => {
+        const img = event.target as HTMLImageElement;
+        const context = this.canvas.getContext('2d');
+        const height = img.naturalHeight || img.offsetHeight || img.height;
+        const width = img.naturalWidth || img.offsetWidth || img.width;
+
+        const dx = width > this.canvas.width ? 0 : (this.canvas.width - width) / 2;
+        const dy = height > this.canvas.height ? 0 : (this.canvas.height - height) / 2;
+
+        // clear our canvas first..
+        clearCanvas(this.canvas);
+
+        // put the image on the canvas
+        context.drawImage(img, 0, 0, width, height, dx, dy, width, height);
+      };
   }
 
   render() {
     return (
       <div class='app-home'>
-        <p>
-          Welcome to the Stencil App Starter.
-          You can use this starter to build entire apps all with
-          web components using Stencil!
-          Check out our docs on <a href='https://stenciljs.com'>stenciljs.com</a> to get started.
-        </p>
 
-        <stencil-route-link url='/profile/stencil'>
-          <button>
-            Profile page
-          </button>
-        </stencil-route-link>
-        <evb-filepicker accept="image/*" onPick={(event) => this.filePicked(event.detail)}>
-          <evb-button>Pick image</evb-button>
-        </evb-filepicker>
+        <div class="tmp">
+          <stencil-route-link url='/profile/stencil'>
+            <button>
+              Profile page
+            </button>
+          </stencil-route-link>
+          <evb-filepicker accept="image/*" onPick={(event) => this.filePicked(event.detail)}>
+            <evb-button>Pick image</evb-button>
+          </evb-filepicker>
+        </div>
+
+        <canvas height={window.innerHeight} width={window.innerWidth} ref={el => this.canvas = el}></canvas>
+
       </div>
     );
   }
